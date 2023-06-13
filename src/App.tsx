ec8191dc1bf6483e4f81e955e9eb93e3b74f69bd
Taras <baranprivat22@gmail.com>
2023-06-13 13:20:42 +0200

    test app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { Navigate, Route, Routes } from "react-router";
-import { lazy } from "react";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Recipes from "./pages/Recipes/Recipes";
+import Recipe from "./pages/Recipe/Recipe";
+import Start from "./pages/Start/Start";
+import Auth from "./pages/Auth/Auth";
+import User from "./pages/User/User";
+import NotFound from "./components/NotFound/NotFound";
+import Liked from "./pages/Liked/Liked";
+// import { lazy } from "react";
 
-const Layout = lazy(() => import("./components/Layout/Layout"));
-const Home = lazy(() => import("./pages/Home/Home"));
-const Auth = lazy(() => import("./pages/Auth/Auth"));
-const Recipes = lazy(() => import("./pages/Recipes/Recipes"));
-const Liked = lazy(() => import("./pages/Liked/Liked"));
-const User = lazy(() => import("./pages/User/User"));
-const Recipe = lazy(() => import("./pages/Recipe/Recipe"));
-const Start = lazy(() => import("./pages/Start/Start"));
-const NotFound = lazy(() => import("./components/NotFound/NotFound"));
+// const Layout = lazy(() => import("./components/Layout/Layout"));
+// const Home = lazy(() => import("./pages/Home/Home"));
+// const Auth = lazy(() => import("./pages/Auth/Auth"));
+// const Recipes = lazy(() => import("./pages/Recipes/Recipes"));
+// const Liked = lazy(() => import("./pages/Liked/Liked"));
+// const User = lazy(() => import("./pages/User/User"));
+// const Recipe = lazy(() => import("./pages/Recipe/Recipe"));
+// const Start = lazy(() => import("./pages/Start/Start"));
+// const NotFound = lazy(() => import("./components/NotFound/NotFound"));
 
 const App = () => {
   return (
